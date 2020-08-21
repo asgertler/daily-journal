854424bd88d850273b1c3d0a75fc73faa3266780
Aaron Gertler <aaron.gertler@gmail.com>
2020-08-21 09:24:54 -0500

    finished chapter 9

---
+++ js/JournalDataProvider.js
@@ -0,0 +1,31 @@
+const journal = [
+    {
+        id: 1,
+        date: "07/24/2025",
+        concept: "HTML & CSS",
+        entry: "We talked about HTML components and how to make grid layouts with Flexbox in CSS.",
+        mood: "Ok"
+    },
+    {
+        id: 2,
+        date: "07/25/2025",
+        concept: "Github",
+        entry: "Started backing out projects up to Githut to prepare for a group project.",
+        mood: "Ok"
+    },
+    {
+        id: 3,
+        date: "07/26/2025",
+        concept: "JavaScript",
+        entry: "Started digging into JS and importing content into HTML files.",
+        mood: "Ok"
+    }
+]
+
+export const useJournalEntries = () => {
+    const sortedByDate = journal.sort(
+        (currentEntry, nextEntry) =>
+            Date.parse(currentEntry.date) - Date.parse(nextEntry.date)
+    )
+    return sortedByDate
+}
