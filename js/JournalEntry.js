6261e6d22f61794d9fab4f79a579bffdc0264456
Aaron Gertler <aaron.gertler@gmail.com>
2020-09-04 16:37:39 -0500

    did some styling and confirmed posting entries works

--- js/JournalEntry.js
+++ js/JournalEntry.js
@@ -6,7 +6,7 @@ export const JournalHTMLConverter = (journalObj) => {
             <div class="entry--title"><h2>${journalObj.title}</h2></div>
             <div class="entry--date"><h3>${new Date(journalObj.date).toLocaleDateString('en-US')}</h3></div>
             <div class="entry--body">${journalObj.entry}</div>
-            <div class="entry--mood">${journalObj.mood}</div>
+            <div class="entry--mood">Mood: ${journalObj.mood}</div>
         </div>
     `
 }
