556072ac9ed9cb1eb3757f0158ed4fa9a1ff2669
Aaron Gertler <aaron.gertler@gmail.com>
2020-09-18 12:48:17 -0500

    added delete button to entries

--- js/JournalEntry.js
+++ js/JournalEntry.js
@@ -4,7 +4,10 @@ export const JournalHTMLConverter = (journalObj) => {
             <div class="entry--title"><h2>${journalObj.title}</h2></div>
             <div class="entry--date"><h3>${new Date(journalObj.date).toLocaleDateString('en-US')}</h3></div>
             <div class="entry--body">${journalObj.entry}</div>
-            <div class="entry--mood">Mood: ${journalObj.mood}</div>
+            <div class="entry--bottom">
+                <div class="entry--mood">Mood: ${journalObj.mood}</div>
+                <button id="deleteNote--${journalObj.id}">Delete</button>
+            </div>
         </div>
     `
 }
